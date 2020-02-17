d34038ae5c938bb17a6b8823c90f464d19f647f4
Hilb3r7 <hoteldehilbert@gmail.com>
2020-02-17 15:58:25 -0700

    Add solution to problem 1

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace Problems
+namespace ProjectEuler100.Problems
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var sw = Stopwatch.StartNew();
 
+            var answer = new Problem001().Solve(1000);
+
+            sw.Stop();
+
+            Console.WriteLine($"anser = {answer}; time - {sw.Elapsed}");
         }
     }
 }
---
+++ Problems/Solutions/Problem001.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler100.Problems
+{
+    // Find the sum of all the multiples of 3 or 5 below 1000
+    class Problem001
+    {
+        public int Solve(int bound)
+        {
+            return SumMultiplesOf(3, bound) + SumMultiplesOf(5, bound) - SumMultiplesOf(3 * 5, bound);
+        }
+
+        private int SumMultiplesOf(int factor, int bound)
+        {
+            var sum = 0;
+            for (int i = factor; i < bound; i += factor)
+            {
+                sum += i;
+            }
+
+            return sum;
+        }
+
+    }
+}
