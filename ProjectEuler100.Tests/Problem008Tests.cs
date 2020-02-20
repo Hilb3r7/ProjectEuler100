72bde4d7195971e66e469fe951bd930b8ac5bdc2
Hilb3r7 <hoteldehilbert@gmail.com>
2020-02-20 12:15:08 -0700

    Add solution to problem 8

---
+++ ProjectEuler100.Tests/Problem008Tests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using ProjectEuler100.Problems;
+
+namespace ProjectEuler100.Tests
+{
+    public class Problem008Tests
+    {
+        [Fact]
+        public void Solve_ShouldSolveExample()
+        {
+            var expected = 5832;
+
+            var actual = new Problem008().Solve(4);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Solve_ShouldSolveProblem()
+        {
+            var expected = 23514624000;
+
+            var actual = new Problem008().Solve(13);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
