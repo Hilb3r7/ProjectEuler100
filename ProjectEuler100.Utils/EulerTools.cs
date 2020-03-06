05b58962fa4152ee854d6da4e6295a5a70058d62
Hilb3r7 <hoteldehilbert@gmail.com>
2020-03-06 12:59:09 -0700

    Add more useful methods

--- ProjectEuler100.Utils/EulerTools.cs
+++ ProjectEuler100.Utils/EulerTools.cs
@@ -26,6 +26,89 @@ namespace ProjectEuler100.Utils
             return n * NChooseK(n - 1, k - 1) / k;
         }
 
+        public int SumOfProperDivisors(int num)
+        {
+            //TODO: If number is big enough, get prime list and use GetPrimeFactorizatoin();
+            var factorization = GetPrimeFactorizationNaively(num);
+            int sum = 1;
+
+            foreach (var prime in factorization.Keys)
+            {
+                sum *= (int)(Math.Pow(prime, factorization[prime] + 1) - 1) / (prime - 1);
+            }
+
+            return sum - num;
+        }
+
+        public int SumOfProperDivisors(int num, List<int> primes)
+        {
+            var factorization = GetPrimeFactorization(num, primes);
+            int sum = 1;
+
+            foreach (var prime in factorization.Keys)
+            {
+                sum *= (int)(Math.Pow(prime, factorization[prime] + 1) - 1) / (prime - 1);
+            }
+
+            return sum - num;
+        }
+
+        public Dictionary<int, int> GetPrimeFactorization(int num, List<int> primes)
+        {
+            var factorizaton = new Dictionary<int, int>();
+
+            if (primes.BinarySearch(num) >= 0) factorizaton.Add(num, 1);
+            else
+            {
+                int index = 0;
+                while (num > 1 && primes[index] * primes[index] <= num)
+                {
+                    int power = 0;
+                    while (num % primes[index] == 0)
+                    {
+                        num /= primes[index];
+                        power++;
+                    }
+
+                    if (power > 0) factorizaton.Add(primes[index], power);
+                    index++;
+                }
+
+                if (num != 1) factorizaton.Add(num, 1);
+            }
+
+            return factorizaton;
+        }
+
+        public Dictionary<int, int> GetPrimeFactorizationNaively(int num)
+        {
+            var factorizaton = new Dictionary<int, int>();
+            int power = 0;
+
+            while (num % 2 == 0)
+            {
+                power++;
+                num /= 2;
+            }
+            if (power != 0) factorizaton.Add(2, power);
+
+            for (int i = 3; i * i <= num; i += 2)
+            {
+                power = 0;
+                while (num % i == 0)
+                {
+                    power++;
+                    num /= i;
+                }
+                if (power != 0) factorizaton.Add(i, power);
+            }
+
+            if (num != 1) factorizaton.Add(num, 1);
+
+            return factorizaton;
+        }
+
+
         private List<int> PrimeSieve(int size)
         {
             var primeList = new List<int>();
